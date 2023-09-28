0ac5cbf61dbc5eed54e4049a725dbfedea178aaa
Aldrick James A. Gicole <144977698+AsashiGodSpeed@users.noreply.github.com>
2023-09-28 20:59:28 +0800

    Add files via upload

---
+++ GICOLE_MinSecConvertermain.py
@@ -0,0 +1,9 @@
+# Enter
+ammount_time = int(input("Enter the amount of time in seconds: "))
+
+# Calculate minutes and seconds
+minutes = ammount_time // 60
+seconds = ammount_time % 60
+
+# Display the result and i dont know why in all letters why F/f is the one able to do like that
+print(F"{total_seconds}seconds is equal to {minutes} minutes and {seconds} seconds.")
