0d06555204bae6d2676245a78a8783e9f938c062
Aldrick James A. Gicole <144977698+AsashiGodSpeed@users.noreply.github.com>
2023-10-02 19:58:06 +0800

    Add files via upload

---
+++ GICOLE_Quiz2.py
@@ -0,0 +1,12 @@
+# PLS PUT THE THSE ONE S UWU r, a, b, c, and d
+r = float(input("Enter the value of r: "))
+a = float(input("Enter the value of a: "))
+b = float(input("Enter the value of b: "))
+c = float(input("Enter the value of c: "))
+d = float(input("Enter the value of d: "))
+
+# calculator.com/GOD
+result = (4 / (3 * (r + 34))) - 9 * (a + b * c) + (3 + d * (2 + a)) / (a + b * d)
+
+# print the Result baby lets GOOOO
+print("Result:", result)
